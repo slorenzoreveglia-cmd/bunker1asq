05e928b0850d6e2c81b5e2bd0800e937ac248901
slorenzoreveglia-cmd <s.lorenzo.reveglia@polofermigiorgi.it>
2026-03-09 11:54:15 +0100

    b

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installato');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Lasciamo che le richieste passino normalmente
-});
